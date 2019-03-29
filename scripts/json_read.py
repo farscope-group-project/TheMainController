0cc95ae7a2546993ba2b2ff1c4ae1f315bea6a98
gabriellamiles <47413041+gabriellamiles@users.noreply.github.com>
2019-03-29 13:40:05 +0000

    Update json_read.py
    Added: can output to separate json file (with success/failure), currently randomly selects item to identify.

--- scripts/json_read.py
+++ scripts/json_read.py
@@ -1,24 +1,69 @@
 import json
+import random
 
-# Read json file
+# Read json file. Use ITEM_FINAL to actually select which item! 
 with open("apc_seeded_2018.json") as json_file:
     json_data = json.load(json_file)
-
     bin_list = json_data['bin_contents']
-    #print(bin_list)
-    for var in bin_list:
-        x = {var['bin_E']}
-        #print(y)
-    # What items are located in bin A
-    items_bin_A = json_data["bin_contents"]["bin_A"]
-    #print(items_bin_A)
+
+    # Delete items in shelves that are impossible to reach
+    del bin_list["bin_J"]
+    del bin_list["bin_K"]
+    del bin_list["bin_L"]
+    
+    # Initiate intermediate dictionary
+    order_items = {}
+    # Make item key, assign value to bin location
+    for key, value in bin_list.items():
+        for i in value:
+                item_in_bin = i
+                order_items[i] = key
+
+    # Initiate dictionary to select from
+    item_final = {}
+    # Delete items that we can't pick up
+    for key,value in order_items.items():
+        if (key == "dr_browns_bottle_brush") \
+           or (key == "kong_air_dog_squeakair_tennis_ball")\
+           or (key == "first_years_take_and_toss_straw_cup")\
+           or (key == "highland_6539_self_stick_notes"):
+            pass
+        else:
+            item_final[key] = value
+
+    print(item_final) 
 
 
 # List of items we would rather not pick up:
-
 # dove box of soap ()
 # set of cups (first_years_take_and_toss_straw_cup?)
 # dog tennis ball (kong_air_dog_squeakair_tennis_ball)
 # green book
 
-# The shelves that are impossible to get to:
+
+# How will the target item be picked?
+pick_an_item = random.randint(0,10)
+target_item = list(item_final.keys())[pick_an_item]
+print(target_item)
+
+# All main movement stuff here?
+
+# Assume that target item is dropped into box
+success = 1
+
+# On completion of movement, delete item from list
+if (success == 1) or (success == 0):
+    del item_final[target_item]
+else:
+    print("Status unknown")
+    # Force a value that equals to failure
+    success = 0
+
+# Create JSON output file. Output if item picked or not.
+item_status = {}
+item_status['output'] = []
+item_status['output'].append({
+    target_item : success
+})
+with open('picked_status_single.json', 'w') as outfile:
+    json.dump(item_status, outfile)
